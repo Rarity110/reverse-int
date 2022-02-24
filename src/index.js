40dde12832bdccc01681a124bebc01775ffbc721
Rarity110 <rarity110@mail.ru>
2022-02-24 13:15:46 +0300

    feat: create function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  let nRev = '';
+    if (String(n)[0] === '-') {
+        nRev = Number([...String(n).slice(1)].reverse().join(''));
+    } else {
+        nRev = Number([...String(n)].reverse().join(''));
+    }
+    return nRev;
 }
+
